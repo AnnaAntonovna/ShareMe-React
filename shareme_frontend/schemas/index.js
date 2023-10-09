f8e6b1069877b1cca2af3f5aff3d41939f1dcd83
Anna Baranova <109595826+AnnaAntonovna@users.noreply.github.com>
2023-10-09 17:05:47 +0200

    Delete shareme_frontend/schemas directory

--- shareme_frontend/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import user from './user'
-import postedBy from './postedBy'
-import comment from './comment'
-import save from './save'
-import pin from './pin'
-export const schemaTypes = [user, postedBy, comment, save, pin]
